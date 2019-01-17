bbbaa56ecbac3a36d46f82d432133638b7e35d8f
Anguandia <mikeanguandia@gmail.com>
2019-01-17 11:25:04 +0300

    remove redundant make_response import

--- app/errors.py
+++ app/errors.py
@@ -1,9 +1,7 @@
-from flask import jsonify, make_response
+from flask import jsonify
 from app.routes.routes import app
 
 
 @app.errorhandler(400)
 def bad_request(error):
-    return make_response(
-      jsonify({'Status': 400, 'error': 'empty request'}), 400
-      )
+    return jsonify({'Status': 400, 'error': 'empty request'}), 400
