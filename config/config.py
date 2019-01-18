f77d4deda4423fa99b9e8e3d8ae3f318912b8e50
Anguandia <mikeanguandia@gmail.com>
2019-01-18 09:56:12 +0300

    revert commit

--- config/config.py
+++ config/config.py
@@ -9,9 +9,12 @@ class Development(Config):
 
 
 class Testing(Config):
-    DEBUG = True
     TESTING = True
+<<<<<<< HEAD
     red_flags = {}
+=======
+    DEBUG = True
+>>>>>>> parent of 73a4201... rearrange config file
 
 
 class Production(Config):
