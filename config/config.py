3e5e172b4284f7ab10e625214d8bf96a43eb5144
Anguandia <mikeanguandia@gmail.com>
2019-01-18 09:08:38 +0300

    clean config file of un needed redflags storages

--- config/config.py
+++ config/config.py
@@ -5,18 +5,17 @@ class Config:
 
 class Development(Config):
     DEBUG = True
-    red_flags = {}
 
 
 class Testing(Config):
-    DEBUG = True
     TESTING = True
-    red_flags = {}
+    DEBUG = True
 
 
 class Production(Config):
     DEBUG = False
     TESTING = False
+
 
 app_config = {
     'DEVELOPMENT': Development, 'TESTING': Testing, 'PRODUCTION': Production
